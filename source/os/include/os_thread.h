f919c9b2b43f989df08ad1dc87f8d3935341c319
nukoseer <uygarkoseer@gmail.com>
2022-09-24 22:39:48 +0300

    Used _WIN32 specific typedefs for ThreadProc

--- source/os/include/os_thread.h
+++ source/os/include/os_thread.h
@@ -1,12 +1,16 @@
 #ifndef H_OS_THREAD_H
 
-typedef uptr OSThreadHandle;
-
 #ifdef _WIN32
-typedef unsigned long OSThreadProcedure(void* parameter);
+typedef unsigned long os_thread_callback_return;
+typedef void* os_thread_callback_param;
+#else
+#error WIN32 must be defined.
 #endif
 
-OSThreadHandle os_thread_create(OSThreadProcedure* thread_procedure, void* parameter);
+typedef uptr OSThreadHandle;
+typedef os_thread_callback_return OSThreadProcedure(os_thread_callback_param parameter);
+
+OSThreadHandle os_thread_create(OSThreadProcedure* thread_procedure, os_thread_callback_param parameter);
 u32            os_thread_resume(OSThreadHandle thread_handle);
 u32            os_thread_suspend(OSThreadHandle thread_handle);
 
