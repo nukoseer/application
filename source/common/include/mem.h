f8ae6c0539f0a34f304983cf1688cfc34983e6cc
nukoseer <uygarkoseer@gmail.com>
2024-01-22 21:02:23 +0300

    add alignment parameter for push_size()

--- source/common/include/mem.h
+++ source/common/include/mem.h
@@ -1,9 +1,17 @@
 #ifndef H_MEMORY_H
 
-#define PUSH_ARRAY(memory_arena, count, type) (push_size(memory_arena, ((count) * sizeof(type))))
-#define PUSH_STRUCT(memory_arena, type) ((type*)(push_size(memory_arena, sizeof(type))))
-#define PUSH_ARRAY_ZERO(memory_arena, count, type) (push_size_zero(memory_arena, ((count) * sizeof(type))))
-#define PUSH_STRUCT_ZERO(memory_arena, type) ((type*)(push_size_zero(memory_arena, sizeof(type))))
+#define DEFAULT_ALIGNMENT 4
+
+#define PUSH_SIZE(memory_arena, size) (push_size(memory_arena, size, DEFAULT_ALIGNMENT))
+#define PUSH_SIZE_N(memory_arena, size, alignment) (push_size(memory_arena, size, alignment))
+#define PUSH_ARRAY(memory_arena, count, type) (push_size(memory_arena, ((count) * sizeof(type)), DEFAULT_ALIGNMENT))
+#define PUSH_ARRAY_N(memory_arena, count, type, alignment) (push_size(memory_arena, ((count) * sizeof(type)), alignment))
+#define PUSH_STRUCT(memory_arena, type) ((type*)(push_size(memory_arena, sizeof(type), DEFAULT_ALIGNMENT)))
+#define PUSH_STRUCT_N(memory_arena, type, alignment) ((type*)(push_size(memory_arena, sizeof(type), alignment)))
+#define PUSH_ARRAY_ZERO(memory_arena, count, type) (push_size_zero(memory_arena, ((count) * sizeof(type)), DEFAULT_ALIGNMENT))
+#define PUSH_ARRAY_ZERO_N(memory_arena, count, type, alignment) (push_size_zero(memory_arena, ((count) * sizeof(type)), alignment))
+#define PUSH_STRUCT_ZERO(memory_arena, type) ((type*)(push_size_zero(memory_arena, sizeof(type), DEFAULT_ALIGNMENT)))
+#define PUSH_STRUCT_ZERO_N(memory_arena, type, alignment) ((type*)(push_size_zero(memory_arena, sizeof(type), alignment)))
 #define STRUCT_ZERO(memory, type) (memory_zero(memory, sizeof(type)))
 
 typedef struct MemoryArena
@@ -23,8 +31,8 @@ MemoryArena* allocate_memory_arena(memory_size max_size);
 void release_memory_arena(MemoryArena* memory_arena);
 TemporaryMemory begin_temporary_memory(MemoryArena* memory_arena);
 void end_temporary_memory(TemporaryMemory temporary_memory);
-void* push_size(MemoryArena* memory_arena, memory_size size);
-void* push_size_zero(MemoryArena* memory_arena, memory_size size);
+void* push_size(MemoryArena* arena, memory_size size, memory_size alignment);
+void* push_size_zero(MemoryArena* arena, memory_size size, memory_size alignment);
 void memory_zero(void* memory, memory_size size);
 
 #define H_MEMORY_H
--- source/common/mem.c
+++ source/common/mem.c
@@ -3,6 +3,19 @@
 #include "os_memory.h"
 #include "mem.h"
 
+// NOTE: Alignment must be power of 2.
+memory_size get_alignment_offset(MemoryArena* arena, memory_size alignment)
+{
+    memory_size alignment_offset = 0;
+
+    if (((memory_size)(u8*)arena->memory + arena->used_size) & alignment)
+    {
+        alignment_offset = (memory_size)((u8*)arena->memory + arena->used_size) & (alignment - 1);
+    }
+
+    return alignment_offset;
+}
+
 MemoryArena* allocate_memory_arena(memory_size max_size)
 {
     MemoryArena* arena = (MemoryArena*)os_memory_allocate(max_size + sizeof(MemoryArena));
@@ -34,22 +47,22 @@ void end_temporary_memory(TemporaryMemory temporary_memory)
     temporary_memory.arena->used_size = temporary_memory.initial_size;
 }
 
-// TODO: Probably, we should also handle alignment in these functions.
-void* push_size(MemoryArena* arena, memory_size size)
+void* push_size(MemoryArena* arena, memory_size size, memory_size alignment)
 {
     void* result = 0;
+    memory_size alignment_offset = get_alignment_offset(arena, alignment);
     
-    ASSERT(arena->used_size + size <= arena->max_size);
-    result = (u8*)arena->memory + arena->used_size;
+    ASSERT(arena->used_size + size + alignment_offset <= arena->max_size);
+    result = (u8*)arena->memory + arena->used_size + alignment_offset;
     
-    arena->used_size += size;
+    arena->used_size += size + alignment_offset;
 
     return result;
 }
 
-void* push_size_zero(MemoryArena* arena, memory_size size)
+void* push_size_zero(MemoryArena* arena, memory_size size, memory_size alignment)
 {
-    void* result = push_size(arena, size);
+    void* result = push_size(arena, size, alignment);
     
     memory_zero(result, size);
 
